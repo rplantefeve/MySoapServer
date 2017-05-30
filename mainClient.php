418921cf2b0d6dd31fab0b67fc0bccd0fdfe822d
rplantefeve <romain.plantefeve@gmail.com>
2017-05-30 17:40:23 +0200

    Suppression commentaires inutiles et corrections mineures

--- mainClient.php
+++ mainClient.php
@@ -1,13 +1,5 @@
 <?php
 
-//$options = array('location' => 'http://localhost/formationphp/soap/mainServer.php',
-//    'uri' => 'http://localhost/test');
-//
-//$client = new SoapClient(NULL, $options);
-//
-//echo $client->getMessage(); //Hello,World! 
-//echo $client->addNumbers(3, 5); // 8
-
 $wsdl = "http://www.webservicex.net/whois.asmx?WSDL";
 $service = new SoapClient($wsdl);
 
@@ -16,9 +8,9 @@ $taballservices = $service->__getFunctions();
 
 print_r($taballservices);
 
-// je choisis d'obtenir tous les taux de change
-$tauxDeChange = $service->GetWhoIS(['HostName' => 'm2iformation.fr']);
+// je choisis d'obtenir les informations sur le domaine microsoft.fr
+$informations = $service->GetWhoIS(['HostName' => 'microsoft.fr']);
 
-file_put_contents("D:/resultats.txt", $tauxDeChange->GetWhoISResult);
+file_put_contents("./resultats.txt", $informations->GetWhoISResult);
 
 
